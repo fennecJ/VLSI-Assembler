76ee0a4edcf0a7e31e1719e2e6b43e9fde920a3f
fennecJ <hwahwa649@gmail.com>
2022-11-11 11:57:14 +0800

    Remove unused variables

--- assembler/ins.c
+++ assembler/ins.c
@@ -12,12 +12,10 @@ char *op_table[] = {
     "ROT" , "HLT",
     "EXT1", 
 };
-int op_len = OP_LEN;
 
 #define CC_LEN sizeof(cc_table)/sizeof(char *)
 char *cc_table[] = {"ALW", "CARRY", "EVEN", "PARITY", "ZERO", "NEG"};
 char *alt_cc_table[] = {"CCA", "CCC", "CCE", "CCP", "CCZ", "CCN"};
-int cc_len = CC_LEN;
 
 int op_find(char *str){
     if(!str)
